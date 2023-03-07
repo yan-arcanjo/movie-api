dc606a1b1c45c94d9b602bcf289bf0cbce208430
Yan <yanarcanjo.dev@gmail.com>
2023-03-07 09:00:22 -0300

    Consumindo API

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,31 +1,28 @@
 import { Container, Movie, MovieList } from "./styles";
+import { APIKey } from "../../config/key";
+import { useState, useEffect } from "react";
 
 function Home() {
 
-    const movies = [
-        {
-            title: 'Spider-man',
-            image_url: 'https://m.media-amazon.com/images/I/91THlJb4lvL.jpg'
-        },
-        {
-            title: 'Spider-man',
-            image_url: 'https://m.media-amazon.com/images/I/91THlJb4lvL.jpg'
-        },
-        {
-            title: 'Spider-man',
-            image_url: 'https://m.media-amazon.com/images/I/91THlJb4lvL.jpg'
-        },
-    ]
+    const [movies, setMovies] = useState([])
+    const image_path = 'https://image.tmdb.org/t/p/w500'
+
+    useEffect(() => {
+
+        fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${APIKey}&language=en-US&page=1`)
+            .then(response => response.json())
+            .then(data => setMovies(data.results))
+    }, [])
 
     return(
         <Container>
-            <h1>Movies</h1>
+            <h1>Popular Movies</h1>
             <MovieList>
                 {
                     movies.map((movie)=>{
                         return(
-                            <Movie>
-                                <a href="https://www.google.com.br"><img src={movie.image_url} alt="spider-man"  /></a>
+                            <Movie key={movie.id}>
+                                <a href="https://www.google.com.br"><img src={`${image_path}${movie.poster_path}`} alt={movie.title}  /></a>
                                 <span>{movie.title}</span>
                             </Movie>
                         )
